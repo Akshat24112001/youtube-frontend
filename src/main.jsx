a40b37e94d8a1ac7b1d558047be10487b3944f12
Akshat24112001 <Akshatsinghrajput24@gmail.com>
2025-09-10 12:02:33 +0530

    Creating route with lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,95 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Provider } from "react-redux";
+
+import { ToastContainer } from "react-toastify";
+
+import "./index.css";
+
+import App from "./App.jsx";
+
+import appStore from "./utiles/appStore.js";
+
+import {
+  HomePage,
+  YourPage,
+  Login,
+  SignUp,
+  Video,
+  CreateChannel,
+  UplaodVideo,
+  Error,
+} from "./utiles/routes.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/channel/:id",
+        element: <YourPage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/video/:id",
+        element: <Video />,
+      },
+      {
+        path: "/createChannel",
+        element: <CreateChannel />,
+      },
+      {
+        path: "/upload",
+        element: <UplaodVideo />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+    <Provider store={appStore}>
+      <RouterProvider router={router} />
+    </Provider>
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+  </StrictMode>
+);
 
